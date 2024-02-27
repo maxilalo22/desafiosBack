48d355a021eccf5672efec2435f9273d44d75fa9
Maximiliano Venditti <maxvenditti94@gmail.com>
2024-02-27 20:05:15 -0300

    error en logger

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,8 @@
-console.log('cargando config')
+
+import { logger } from '../utils/logger.js'
+
+
+//console.log('cargando config')
 
 //export const MODO_EJECUCION = 'online'
 export const MODO_EJECUCION = 'offline'
@@ -6,3 +10,11 @@ export const MODO_EJECUCION = 'offline'
 export const PORT = 8080
 
 
+
+export const loggerLevel = {
+    CONSOLE: MODO_EJECUCION === 'offline' ? 'error': 'http',
+    FILE: MODO_EJECUCION === 'offline' ? 'http' : 'error'
+}
+//export const NODE_ENV  = process.env.NODE_ENV || 'development'
+
+//logger.info('configuración cargada')
--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,14 @@
 //import { connect as connectToMongoose } from 'mongoose'
 import { MODO_EJECUCION } from '../config/config.js'
+import { logger } from '../utils/logger.js'
 
 export async function connect() {
     if (MODO_EJECUCION === 'online') {
         await connectToMongoose(MONGODB_CNX_STR)
-        console.log('conectado a mongodb')
+        logger.info('conectado a mongodb')
+        //console.log('conectado a mongodb')
     } else {
-        console.log('trabajando con persistencia local')
+        logger.info('trabajando con persistencia local')
+        //console.log('trabajando con persistencia local')
     }
 }
